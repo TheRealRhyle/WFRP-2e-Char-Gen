5b2a8d9d25c70852fab8e54b271d4dc453a46bf3
Phillip Welch <pwelch01@tampabay.rr.com>
2018-08-01 22:37:30 -0400

    cleaned PEP8 warnings

--- careers.py
+++ careers.py
@@ -3,13 +3,27 @@ from random import randint
 
 def starting_career(race):
     dict_careers = {
-        'dwarf': ['Agitator', 'Bodyguard', 'Burgher', 'Coachman', 'Entertainer', 'Jailer', 'Marine', 'Mercenary', 'Militiaman', 'Miner', 'Noble', 'Outlaw', 'Pit Fighter', 'Protagonist', 'Rat Catcher', 'Runebearer', 'Scribe', 'Seaman', 'Servant', 'Shieldbreaker', 'Smuggler', 'Soldier', 'Student', 'Thief', 'Toll Keeper', 'Tomb Robber', 'Tradesman', 'Troll Slayer', 'Watchman'],
-        'elf': ['Apprentice Wizard', 'Entertainer', 'Envoy', 'Hunter', 'Kithband Warrior', 'Mercenary', 'Messanger', 'Outlaw', 'Outrider', 'Rogue', 'Scribe', 'Seaman', 'Student', 'Thief', 'Tradesman', 'Vagabond'],
-        'halfling': ['Agitator', 'Barber-Surgeon', 'Bone Picker', 'Bounty Hunter', 'Burgher', 'Camp Follower', 'Charcoal-Burner', 'Entertainer', 'Ferryman', 'Fieldwarden', 'Fisherman', 'Grave Tobber', 'Hunter', 'Mercenary', 'Messanger', 'Militiaman', 'Outlaw', 'Peasant', 'Rat Catcher', 'Rogue', 'Servant', 'Smuggler', 'Soldier', 'Student', 'Thief', 'Toll Keeper', 'Tomb Robber', 'Tradesman', 'Vagabond', 'Valet', 'Watchman'],
-        'human': ['Agitator', 'Apprentice Wizard', 'Bailiff', 'Barber-Surgeon', 'Boatman', 'Bodyguard', 'Bone Picker', 'Bounty Hunter', 'Burgher', 'Camp Follower', 'Charcoal-Burner', 'Coachman', 'Entertainer', 'Estalian Diestro', 'Ferryman', 'Fisherman', 'Grave Tobber', 'Hedge Wizard', 'Hunter', 'Initiate', 'Jailer', 'Kislevite Kossar', 'Marine', 'Mercenary', 'Messanger', 'Militiaman', 'Miner', 'Noble', 'Norse Erserker', 'Outlaw', 'Outrider', 'Peasant', 'Pit Fighter', 'Protagonist', 'Rat Catcher', 'Roadwarden', 'Rogue', 'Scribe', 'Seaman', 'Servant', 'Smuggler', 'Soldier', 'Squire', 'Student', 'Thief', 'Thug', 'Toll Keeper', 'Tomb Robber', 'Tradesman', 'Vagabond', 'Valet', 'Watchman', 'Woodsman', 'Zealot']
+        'dwarf': ['Agitator', 'Bodyguard', 'Burgher', 'Coachman', 'Entertainer', 'Jailer', 'Marine', 'Mercenary',
+                  'Militiaman', 'Miner', 'Noble', 'Outlaw', 'Pit Fighter', 'Protagonist', 'Rat Catcher', 'Runebearer',
+                  'Scribe', 'Seaman', 'Servant', 'Shieldbreaker', 'Smuggler', 'Soldier', 'Student', 'Thief',
+                  'Toll Keeper', 'Tomb Robber', 'Tradesman', 'Troll Slayer', 'Watchman'],
+        'elf': ['Apprentice Wizard', 'Entertainer', 'Envoy', 'Hunter', 'Kithband Warrior', 'Mercenary', 'Messenger',
+                'Outlaw', 'Outrider', 'Rogue', 'Scribe', 'Seaman', 'Student', 'Thief', 'Tradesman', 'Vagabond'],
+        'halfling': ['Agitator', 'Barber-Surgeon', 'Bone Picker', 'Bounty Hunter', 'Burgher', 'Camp Follower',
+                     'Charcoal-Burner', 'Entertainer', 'Ferryman', 'Fieldwarden', 'Fisherman', 'Grave Robber', 'Hunter',
+                     'Mercenary', 'Messenger', 'Militiaman', 'Outlaw', 'Peasant', 'Rat Catcher', 'Rogue', 'Servant',
+                     'Smuggler', 'Soldier', 'Student', 'Thief', 'Toll Keeper', 'Tomb Robber', 'Tradesman', 'Vagabond',
+                     'Valet', 'Watchman'],
+        'human': ['Agitator', 'Apprentice Wizard', 'Bailiff', 'Barber-Surgeon', 'Boatman', 'Bodyguard', 'Bone Picker',
+                  'Bounty Hunter', 'Burgher', 'Camp Follower', 'Charcoal-Burner', 'Coachman', 'Entertainer',
+                  'Estalian Diestro', 'Ferryman', 'Fisherman', 'Grave Tobber', 'Hedge Wizard', 'Hunter', 'Initiate',
+                  'Jailer', 'Kislevite Kossar', 'Marine', 'Mercenary', 'Messenger', 'Militiaman', 'Miner', 'Noble',
+                  'Norse Berserker', 'Outlaw', 'Outrider', 'Peasant', 'Pit Fighter', 'Protagonist', 'Rat Catcher',
+                  'Roadwarden', 'Rogue', 'Scribe', 'Seaman', 'Servant', 'Smuggler', 'Soldier', 'Squire', 'Student',
+                  'Thief', 'Thug', 'Toll Keeper', 'Tomb Robber', 'Tradesman', 'Vagabond', 'Valet', 'Watchman',
+                  'Woodsman', 'Zealot']
     }
-
     rand_career = randint(0, len(dict_careers[race]) - 1)
-    startingCareer = dict_careers[race][rand_career]
+    startingcareer = dict_careers[race][rand_career]
 
-    return startingCareer
+    return startingcareer
--- genchar.py
+++ genchar.py
@@ -7,6 +7,7 @@ import careers
 import personal_details as pd
 import races
 import skills
+import career_listf as cl
 
 race = ['dwarf', 'elf', 'halfling', 'human']
 gender = ('male', 'female')
@@ -30,9 +31,23 @@ def build_random_char():
     ski = sorted(ski)
     tal = sorted(tal)
 
+    mp_key, mp_value, sp_key, sp_value = random_stat_blocks(rand_race)
+
+    # Select random career
+    sel_career_name = careers.starting_career(rand_race)
+
+    #get career info for selected career
+    sel_career = cl.career_selection(sel_career_name + ' Advance Scheme')
+    mp_adv = list(sel_career['Statblock'])[0:8]
+    sec_adv = list(sel_career['Statblock'])[7:]
+    mp_adv = '\t'.join(str(li) for li in mp_adv)
+    sec_adv = '\t'.join(str(li) for li in sec_adv)
+    #sel_career['Skills'] = '\t'.join(sel_career['Skills'])
+    #sel_career['Talents'] = '\t'.join(sel_career['Talents'])
+
     charout = {}
     charout['name'] = pd.get_names(rand_race, rand_gender)
-    charout['career'] = careers.starting_career(rand_race)
+    charout['career'] = sel_career_name
     charout['race'] = rand_race.title()
     charout['gender'] = rand_gender.title()
     charout['height'] = pd.height(rand_race, rand_gender)
@@ -44,9 +59,19 @@ def build_random_char():
     charout['siblings'] = pd.number_siblings(rand_race)
     charout['birthplace'] = pd.birthplace(rand_race)
     charout['starsign'] = pd.star_sign()
-    charout['starting_stat_block'] = random_stat_blocks(rand_race)
     charout['skills'] = sorted(ski)
     charout['talents'] = sorted(tal)
+    charout['main_profile_header'] = mp_key
+    charout['main_profile'] = mp_value
+    charout['main_profile_adv'] = mp_adv
+    charout['secondary_profile_header'] = sp_key
+    charout['secondary_profile'] = sp_value
+    charout['secondary_profile_adv'] = sec_adv
+    charout['adv_skill'] = sel_career['Skills']
+    charout['adv_talent'] = sel_career['Talents']
+    charout['trappings'] = sel_career['Trappings']
+
+    #charout['starting_stat_block'] = random_stat_blocks(rand_race)
 
     return charout
 
@@ -56,7 +81,7 @@ def random_stat_blocks(race):
     mp, sp = races.profiles(race)
 
     # Get starting Skills and Talents based on rand_race
-    ski, tal = skills.skills_talents(race)
+    #ski, tal = skills.skills_talents(race)
 
     mp_key, mp_value = '', ''
     for key, value in mp.items():
@@ -72,18 +97,7 @@ def random_stat_blocks(race):
     for key, value in sp.items():
         sp_value = str(sp_value) + str(value) + '\t'
 
-    main_prof = """\n    ========= Main Profile =========
-    {}
-    {}
-
-    ====== Secondary Profile =======
-    {}
-    {}
-    """.format(mp_key, mp_value, sp_key, sp_value)
-
-    profile = main_prof  # + '\n' + sec_prof
-
-    return profile
+    return (mp_key, mp_value, sp_key, sp_value)
 
 def save_character(charout):
     with open('characters\{}.dat'.format(charout['name']), 'wb') as f:
@@ -98,9 +112,12 @@ def load_character(char):
 def format_sheet(charin):
     charin['weight'] = str(charin['weight'])
 
-    skills = charin['skills']
+    #skills = charin['skills']
     charin['skills'] = '\n'.join(charin['skills'])
     charin['talents'] = '\n'.join(charin['talents'])
+    charin['adv_skill'] = ', '.join(charin['adv_skill'])
+    charin['adv_talent'] = ', '.join(charin['adv_talent'])
+    charin['trappings'] = ', '.join(charin['trappings'])
 
     sheet = """Name: {name} the {career}
     Race: {race:15s}Gender: {gender:15s}Age: {age}
@@ -109,14 +126,35 @@ def format_sheet(charin):
     Birthplace: {birthplace}
     Star Sign: {starsign}
     Distinguishing Marks: {marks}
-    {starting_stat_block}
+    
+    Main Profile:\t{main_profile_header}
+    Main Profile:\t{main_profile}
+    Main Advance:\t{main_profile_adv}
+    
+    Sec Profile:\t{secondary_profile_header}
+    Sec Profile:\t{secondary_profile}
+    Sec Advance:\t{secondary_profile_adv}
+    
+    Trappings:
+    -------
+    {trappings}
+    
     Skills:
     -------
     {skills}
-    
+       
     Talents:
     -------
     {talents}
+    
+    Skill Advances:
+    -------
+    {adv_skill}
+    
+    Talent Advances:
+    -------
+    {adv_talent}
+    
     """.format(**charin).replace('    ','')
 
     return sheet
@@ -125,7 +163,8 @@ def mainloop(option):
     if option.lower() == 'g':
         charout = build_random_char()
         print(format_sheet(charout))
-        ser = input('[S]ave,[E]xit,[R]eroll')
+        ser = input('[S]ave,[E]xit,[R]eroll: ')
+        print('')
         if ser.lower() == 's':
             save_character(charout)
             saved=('Character: {} has been saved to characters\{}.dat').format(charout['name'],charout['name'])
@@ -145,4 +184,5 @@ def mainloop(option):
 
 if __name__=='__main__':
     option = input('Would you like to [G]enerate a Random Character or [L]oad a character?')
+    print('')
     mainloop(option)
